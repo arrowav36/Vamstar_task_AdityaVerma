323267d54306b54a5bf6cf1a375cbecd409a2e1f
Aditya Verma <arrowav36@gmail.com>
2021-02-09 21:05:49 +0530

    Bidding_date extracted sucessfully

--- constants.js
+++ constants.js
@@ -4,3 +4,5 @@ document.querySelector('#block-views-block-view-noticia-pbh-block-5 > div > div 
 // Object
 document.querySelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > p:nth-child(6) > font > font > font > font').innerText
 
+// Bidding Date
+document.querySelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(19) > font > font > font > font').innerText
