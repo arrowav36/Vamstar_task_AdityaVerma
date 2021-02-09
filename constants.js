b3a903f099d74f319df97d362038be5820a82b62
Aditya Verma <arrowav36@gmail.com>
2021-02-09 21:06:46 +0530

    Links also extracted from the webpage

--- constants.js
+++ constants.js
@@ -6,3 +6,13 @@ document.querySelector('#block-views-block-view-noticia-pbh-block-5 > div > div 
 
 // Bidding Date
 document.querySelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(19) > font > font > font > font').innerText
+
+// LINK 1
+document.querySelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-field-historico-da-licitacao > div > table > tbody > tr:nth-child(1) > td:nth-child(2) > div > div > div > a').href
+
+// LINK 2
+document.querySelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-field-historico-da-licitacao > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > div > div > div > a').href
+
+// LINK 3
+document.querySelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-field-historico-da-licitacao > div > table > tbody > tr:nth-child(3) > td:nth-child(2) > div > div > div > a').href
+
