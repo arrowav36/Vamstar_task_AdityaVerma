8a62981ac2cbbb0ce0562d5dc34a5ea085c8b552
Aditya Verma <arrowav36@gmail.com>
2021-02-09 21:04:55 +0530

    publication_date and objects extracted

---
+++ constants.js
@@ -0,0 +1,6 @@
+// Publication Date
+document.querySelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > span:nth-child(1) > font > font > font > font').innerText
+
+// Object
+document.querySelector('#block-views-block-view-noticia-pbh-block-5 > div > div > div > div > div > div.views-field.views-field-nothing > span > p:nth-child(6) > font > font > font > font').innerText
+
